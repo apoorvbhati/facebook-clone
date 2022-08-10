e9bbe703d8800cb35d16b11d805e17a6990287cc
apoorvbhati <52211016+apoorvbhati@users.noreply.github.com>
2022-08-10 14:51:00 +0530

    added the sending mails funcionality

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,13 +1,15 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const {
   validateEmail,
   validateLength,
   validateUsername,
 } = require("../helpers/validation");
 const { generateToken } = require("../helpers/tokens");
+const { sendVerificationMail } = require("../helpers/mailer");
 
-const home = async (req, res) => {
+exports.register = async (req, res) => {
   try {
     const {
       first_name,
@@ -73,12 +75,74 @@ const home = async (req, res) => {
       { id: user._id.toString() }, // the id is converted to a string
       "30m"
     );
-    console.log(emailVerificationToken);
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`; // this url is the one that is hit when the user clicks on the confirm you account in the mail
+    sendVerificationMail(user.email, user.first_name, url);
 
-    res.json(user);
+    // the token needs to be sent to the frontend so that the user is able to login
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      picture: user.picture,
+      token: token,
+      verified: user.verified,
+      message: "Register success! Please verify your email to start",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-module.exports = home;
+// This function activates the user account
+exports.activateAccount = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const user = jwt.verify(token, process.env.TOKEN_SECRET); // verify the jwt
+    const check = await User.findById(user.id); // find the user by id and then check whether he is verified or not
+
+    if (check.verified == true) {
+      res.status(400).json({ message: "This email is already acitvated" });
+    } else {
+      await User.findByIdAndUpdate(user.id, { verified: true });
+      res
+        .status(200)
+        .json({ message: "Account has been activated successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "The email you entered is not connected to an account",
+      });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    if (!check) {
+      return res.status(400).json({
+        message: "Invalid credentials. Please try again.",
+      });
+    }
+
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      picture: user.picture,
+      token: token,
+      verified: user.verified,
+      message: "Register success! Please verify your email to start",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,5 @@
-function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000");
-    console.log(res);
-  };
-
-  get();
-
-  return <div>welcome to frontend</div>;
-}
+const App = () => {
+  return <h1>hi</h1>;
+};
 
 export default App;
